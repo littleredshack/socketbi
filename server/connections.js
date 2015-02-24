f860f2b6b69941d92bd12834c5b06e2625134a88
littleredshack <littleredshack@gmail.com>
2015-02-24 21:24:00 +0000

    Cleanup of connections.js

--- server/connections.js
+++ server/connections.js
@@ -6,27 +6,6 @@ NCONF is not dynamic so if new databases are added
 to file then they are not picked up
 So writing something to read config file into object
 ******************************************************/
-
-/*
-module.exports = function User(data) {
-
-    //this properly private stuff.
-
-    var privateVar;
-    var username = data.username;
-    var pass = data.pass; //etc
-
-    function privateFunc() {
-    }
-
-    return {
-        login: function(cb) {
-            db.doStuff(username, pass, cb);
-        }
-    };
-};
-*/
-
 exports.dblist = function dblist(cfgfile,callback) {
 	var dblist = [];
 	fs.readFile(cfgfile, 'utf8', function (err,contents) {
@@ -58,11 +37,22 @@ exports.db1 = createDBConnection("mysql1");
 
 
 /*
-readConfig('datasources.json' , function(c) {
-	var dblist = [];
-	c.datasources.forEach(function(entry){
-		dblist.push(entry.name);
-	});
-	console.log(dblist);
-}
+module.exports = function User(data) {
+
+    //this properly private stuff.
+
+    var privateVar;
+    var username = data.username;
+    var pass = data.pass; //etc
+
+    function privateFunc() {
+    }
+
+    return {
+        login: function(cb) {
+            db.doStuff(username, pass, cb);
+        }
+    };
+};
 */
+
